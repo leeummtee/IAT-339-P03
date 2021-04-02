b1ea5a3cfa57f0f1ab656574a7accd15fc108284
Liam <leeummtee@gmail.com>
2021-04-01 19:09:41 -0700

    Testing bg color change on scroll

--- js/main.js
+++ js/main.js
@@ -1,24 +1,49 @@
-/**
- * This was built using the scrollie jQuery Plugin
- * https://github.com/Funsella/jquery-scrollie
- */
+// background color changing while scrolling
+// reference: https://codepen.io/patrickcameron/pen/WNGrpEw
+// $(function() {
+//   $(window).scroll(function() {
+//     if( $(this).scrollTop() > 600 ) {
+//       $('body').addClass('changeColorGrey');
+//     } else {
+//       $('body').removeClass('changeColorGrey');
+//     }
+//   })
+// })
+
+$(function() {
+  $(window).scroll(function() {
+    if( $(this).scrollTop() > 600 ) {
+      $('body').addClass('changeColorGrey');
+
+    // } else if( $(this).scrollTop() > 1200 ) {
+    //   $('body').removeClass('changeColorGrey');
+    //   $('body').addClass('changeColorWhite');
+
+    } else {
+      $('body').removeClass('changeColorGrey');
+    }
+  })
+})
+
+$(function() {
+  $(window).scroll(function() {
+    if( $(this).scrollTop() > 1200 ) {
+      $('body').removeClass('changeColorGrey');
+      $('body').addClass('changeColorWhite');
+
+    } else {
+      $('body').removeClass('changeColorGrey');
+      $('body').addClass('changeColorWhite');
+    }
+  })
+})
 
 
-$( window ).ready(function() {
-
-    var wHeight = $(window).height();
-
-    $('.slide')
-      .height(wHeight)
-      .scrollie({
-        scrollOffset : -50,
-        scrollingInView : function(elem) {
-
-          var bgColor = elem.data('background');
-
-          $('body').css('background-color', bgColor);
-
-        }
-      });
-
-  });
+//making the arrow dissapear while scrolling
+// $(function() {
+//   $(window).scroll(function() {
+//     if( $(this).scrollTop() > 200 ) {
+//       removeClass('arrow');
+//     }
+//   })
+// })
